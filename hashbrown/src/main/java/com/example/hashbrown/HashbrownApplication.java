65a703f08ff12cf830909e2f3861382b309dec12
Matthew Fevold <matt.fevold@gmail.com>
2022-10-14 18:05:47 -0500

    init spring initializer

---
+++ hashbrown/src/main/java/com/example/hashbrown/HashbrownApplication.java
@@ -0,0 +1,13 @@
+package com.example.hashbrown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashbrownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashbrownApplication.class, args);
+	}
+
+}
---
+++ hashbrown/src/test/java/com/example/hashbrown/HashbrownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hashbrown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashbrownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
